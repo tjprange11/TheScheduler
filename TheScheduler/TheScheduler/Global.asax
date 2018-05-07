d0ed050269bce17424482449139c3442821dbb67
TJ Prange <tjprange11@gmail.com>
2018-05-07 10:42:08 -0500

    initial commit

---
+++ TheScheduler/TheScheduler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheScheduler.MvcApplication" Language="C#" %>
---
+++ TheScheduler/TheScheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheScheduler.Startup))]
+namespace TheScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
