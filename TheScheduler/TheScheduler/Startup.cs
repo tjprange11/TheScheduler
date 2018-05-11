420642a8facea2d543789926eaff783ff1f45dfe
Austin Pichler <austin.pichler@gmail.com>
2018-05-11 14:30:36 -0500

    heavy changes to seed method

--- TheScheduler/TheScheduler/Startup.cs
+++ TheScheduler/TheScheduler/Startup.cs
@@ -13,41 +13,6 @@ namespace TheScheduler
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-
-            
-
-            //var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-            //string userPWD;
-
-            //var user = new ApplicationUser();
-            //user.UserName = "tjprange11";
-            //user.Email = "tjprange11@gmail.com";
-            //userPWD = "RoCkS=13";
-
-            //if (UserManager.Create(user, userPWD).Succeeded)
-            //{
-            //    UserManager.AddToRole(user.Id, "Admin");
-            //}
-
-            //user = new ApplicationUser();
-            //user.UserName = "SlyFiye";
-            //user.Email = "austin.pichler@gmail.com";
-            //userPWD = "Password100.";
-
-            //if (UserManager.Create(user, userPWD).Succeeded)
-            //{
-            //    UserManager.AddToRole(user.Id, "Admin");
-            //}
-
-            //user = new ApplicationUser();
-            //user.UserName = "Air-Ick";
-            //user.Email = "EHenderson421@gmail.com";
-            //userPWD = "Password100.";
-
-            //if (UserManager.Create(user, userPWD).Succeeded)
-            //{
-            //    UserManager.AddToRole(user.Id, "Admin");
-            //}
         }
     }
 }
